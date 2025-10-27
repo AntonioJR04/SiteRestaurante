e64b5cb32f5b707d7887cbd6babc8b9311158863
JuniiorL <antonioleitej@hotmail.com>
2025-10-27 18:47:19 -0300

    header atualizado

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,24 +1,46 @@
-
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import '../style/header.css';
-import { FaPizzaSlice } from 'react-icons/fa';
-import '../style/header.css';
+import { FaPizzaSlice } from "react-icons/fa";
+import "../style/header.css";
+
 export default function Header() {
   const navigate = useNavigate();
 
   return (
-    <header className="header">
-      <h1 className="logo">ComandaPro</h1>
+    <header className="header-glass">
+      <div className="logo-area" onClick={() => navigate("/")}>
+        {/* <FaPizzaSlice size={26} color="#e60023" /> */}
+        <h1>ComandaPro</h1>
+      </div>
+
       <nav className="navbar">
-        <ul className="navList">
-          <li><a href="#cardapio" className="navLink" onClick={(e) => { e.preventDefault(); navigate("/"); }}>Home</a></li>
-          <li><a href="#reservas" className="navLink" onClick={(e) => { e.preventDefault(); navigate("/reservas"); }}>Reservar</a></li>
-          <li><a href="#pedidos" className="navLink" onClick={(e) => { e.preventDefault(); navigate("/pratos"); }}>Pratos</a></li>
-          <li><a href="#avaliacoes" className="navLink" onClick={(e) => { e.preventDefault(); navigate("/bebidas"); }}>Bebidas</a></li>
+        <ul className="nav-list">
+          <li>
+            <a href="#" onClick={(e) => { e.preventDefault(); navigate("/"); }}>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#" onClick={(e) => { e.preventDefault(); navigate("/reservas"); }}>
+              Reservar
+            </a>
+          </li>
+          <li>
+            <a href="#" onClick={(e) => { e.preventDefault(); navigate("/pratos"); }}>
+              Pratos
+            </a>
+          </li>
+          <li>
+            <a href="#" onClick={(e) => { e.preventDefault(); navigate("/bebidas"); }}>
+              Bebidas
+            </a>
+          </li>
         </ul>
       </nav>
+
+      <div className="profile-icon" onClick={() => navigate("/profile")}>
+        <FaPizzaSlice size={24} color="#694100" />
+      </div>
     </header>
   );
 }
-
