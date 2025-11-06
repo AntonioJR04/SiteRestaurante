74ccda9619dc0593e911e7a0cb6e9d75d557d51f
AntonioJR04 <802.557@alunos.unigran.br>
2025-11-06 17:25:59 -0300

    Revert "Junção João Front com a Main"

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,46 +1,24 @@
+
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { FaPizzaSlice } from "react-icons/fa";
-import "../style/header.css";
-
+import '../style/header.css';
+import { FaPizzaSlice } from 'react-icons/fa';
+import '../style/header.css';
 export default function Header() {
   const navigate = useNavigate();
 
   return (
-    <header className="header-glass">
-      <div className="logo-area" onClick={() => navigate("/")}>
-        {/* <FaPizzaSlice size={26} color="#e60023" /> */}
-        <h1>ComandaPro</h1>
-      </div>
+    <header className="cabecalho">
+      <a href="#" onClick={(e) => { e.preventDefault(); navigate("/"); }}><p>Home</p></a>
+      <a href="#" onClick={(e) => { e.preventDefault(); navigate("/reservas"); }}><p>Reservar</p></a>
+      <a href="#" onClick={(e) => { e.preventDefault(); navigate("/pratos"); }}><p>Pratos</p></a>
+      <a href="#" onClick={(e) => { e.preventDefault(); navigate("/bebidas"); }}><p>Bebidas</p></a>
 
-      <nav className="navbar">
-        <ul className="nav-list">
-          <li>
-            <a href="#" onClick={(e) => { e.preventDefault(); navigate("/"); }}>
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#" onClick={(e) => { e.preventDefault(); navigate("/reservas"); }}>
-              Reservar
-            </a>
-          </li>
-          <li>
-            <a href="#" onClick={(e) => { e.preventDefault(); navigate("/pratos"); }}>
-              Pratos
-            </a>
-          </li>
-          <li>
-            <a href="#" onClick={(e) => { e.preventDefault(); navigate("/bebidas"); }}>
-              Bebidas
-            </a>
-          </li>
-        </ul>
-      </nav>
+      <FaPizzaSlice className="icone-profile" onClick={(e) => 
+        { e.preventDefault(); navigate("/profile"); }} size={30} color="#E60023"/>
 
-      <div className="profile-icon" onClick={() => navigate("/profile")}>
-        <FaPizzaSlice size={24} color="#694100" />
-      </div>
+
+
     </header>
   );
 }
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,50 +1,3 @@
-
-// import '../style/login.css'
-
-// export default function Login() {
-
-//   return (
-//     <div className="login-container">
-//       <form className="login-form">
-//         <h2>Entrar</h2>
-
-//         <label>
-//           Usuário:
-//           <input 
-//             placeholder="Digite seu e-mail ou telefone" 
-//             type="text" 
-//             name="username" 
-//             required 
-//           />
-//         </label>
-
-//         <label>
-//           Senha:
-//           <input 
-//             placeholder="Digite sua senha" 
-//             type="password" 
-//             name="password" 
-//             required 
-//           />
-//         </label>
-
-//         <div className="lembrar">
-//           <input className="checkbox-lembrarme" type="checkbox" id="lembrar" />
-//           <label htmlFor="lembrar">Lembrar-me</label>
-//         </div>
-
-//         <button type="submit">Login</button>
-
-//         <div className="signup-text">
-//           <h6>Não possui conta?</h6>
-//           <a href="/signup">Cadastre-se</a>
-//         </div>
-//       </form>
-//       <div className="login-image"></div>
-//     </div>
-//   )
-// }
-
 import { useState } from "react";
 import "../style/login.css";
 import axios from 'axios';
@@ -142,4 +95,3 @@ export default function Login() {
     </div>
   );
 }
-
